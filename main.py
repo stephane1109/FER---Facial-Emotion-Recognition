58b98ce630956a8363f9db1ff35e65ddacd280d5
Stéphane Meurisse <141574089+stephane1109@users.noreply.github.com>
2024-10-04 15:52:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 # Licence : Ce programme est un logiciel libre : vous pouvez le redistribuer selon les termes de la Licence Publique Générale GNU v3
 ##########################################
 
-# pip install opencv-python streamlit pytube fer pandas matplotlib tensorflow xlsxwriter
+# pip install opencv-python streamlit fer pandas matplotlib tensorflow xlsxwriter
 # pip install tensorflow-gpu ##### si gpu
 # pip install watchdog
 # pip install yt_dlp
